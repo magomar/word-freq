4a6bf4b6fb07e5df3a150080435a5929e93ff7aa
magomar <magomar@gmail.com>
2019-02-15 12:26:04 +0100

    Adds functionality to compute word frequencies

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import os
 import requests
+import operator
+import re
+import nltk
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 from models import Result
@@ -21,12 +26,48 @@ def index():
         # get url that the user has entered
         try:
             url = request.form['url']
+            limit = int(request.form['num_words'])
             r = requests.get(url)
-            print(r.text)
         except:
             errors.append(
                 "Unable to get URL. Please make sure it's valid and try again."
             )
+            return render_template('index.html', errors=errors)
+        if r: 
+            # Text processing block
+            # Obtain raw text by removing html tags
+            raw = BeautifulSoup(r.text, 'html.parser').get_text() 
+            # Set path to the Natural Language Toolkit data folder, which contains the tokenizers
+            nltk.data.path.append('./nltk_data/')  
+            # Fet tokens from raw text using 
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            # Regular expression for any word including at least one alphanumeric character
+            # This will exclude punctuation and numbers
+            nonPunct = re.compile('.*[A-Za-z].*')
+            # Count words matching the regultar expresion above
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+            # Remove stop words (prepositions, pronouns, etc.)
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            # Count non stop words
+            no_stop_words_count = Counter(no_stop_words)
+            # Save the results to dictionary with words as keys and frequencies as values
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )[:limit]
+            try:
+                result = Result(
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
     return render_template('index.html', errors=errors, results=results)
 
 
